a11c48f7e1bc99cde2bea7c71e7d875e3dfa677a
sudsek6060 <sekhar.sudhansu6060@gmail.com>
2023-02-27 19:42:59 +0530

    js added

--- script.js
+++ script.js
@@ -16,36 +16,62 @@ const randomFunc = {
 }
 // 
 clipboardEl.addEventListener('click', () => {
-    
+    // clipboardEl.select()
+    // document.execCommand('copy');
 })
 
 generateEl.addEventListener('click', () => {
-    
+    const valu = generatePassword();
+    resultEl.textContent = valu;
 })
 
 function generatePassword(lower, upper, number, symbol, length) {
-  
+   length  = Math.floor((lengthEl.value)/4);
+   let password = ''
+    for (let i = 0; i < length; i++) {
+        password += getRandomUpper() + getRandomLower() + getRandomSymbol() + getRandomNumber()
+    }
+    return password;
    
 }
 
 
 function getRandomLower() {
+    const lower = 'abcdefghijklmnopqrstuvwxyz';
+    let lowerChars = '';
     
+     lowerChars += lower.charAt(Math.floor(Math.random() * lower.length))        
+    
+    return lowerChars
     
 }
 
 function getRandomUpper() {
+    const upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let upperChars = '';
     
+     upperChars += upper[Math.floor(Math.random() * upper.length)]        
+    
+    return upperChars;
     
 }
 
 
 function getRandomNumber() {
-   
+    const num = '1234567890';
+    let nums = '';
+    
+     nums += num[Math.floor(Math.random() * num.length)]        
+    
+    return nums;
+    
 }
 
 
 function getRandomSymbol() {
-    
+    const sym = '!@#$&^%'
+    let syms = '';
+    return syms = sym[Math.floor(Math.random() * sym.length)]
+   
 }
 
